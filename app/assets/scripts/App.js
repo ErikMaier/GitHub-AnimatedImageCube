cd250ed82d8776f11875933f50f7f227bc0438fd
Erik Maier <erikmaier80@gmail.com>
2019-12-20 17:04:09 +0100

    Initial commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,15 @@
+import '../styles/styles.css';
+import 'lazysizes'
+
+//React Related Code starts
+
+//Import React components
+//import Component from './modules/ReactComponent'
+
+//ReactDOM.render(<Component />, document.querySelector("#react"))
+//React Related Code ends
+
+if (module.hot) {
+    module.hot.accept()
+}
+
